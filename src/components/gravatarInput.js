24ba46f89784d5b731618d3ac09603e314e10d5e
Yesid Ghisays <yeso126@hotmail.com>
2019-07-30 19:39:22 -0500

    First commit

---
+++ src/components/gravatarInput.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class gravatarInput extends React.Component {
+    constructor (){
+        super()
+        this.state = {}
+    }
+}
+
+export default gravatarInput;
